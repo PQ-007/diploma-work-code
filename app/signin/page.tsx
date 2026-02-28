432e419ce3f7c8988c5db4938dad6454e3929b0e
PQ-007 <bilguuntushig.a@gmail.com>
2026-02-28 17:00:34 +0800

    fix: wrap LoginForm in Suspense on /signin

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,13 @@
-import LoginForm from '@/components/LoginForm'
+import { Suspense } from "react";
+import LoginForm from "@/components/LoginForm";
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex flex-col justify-center">
-      <LoginForm />
+      {/* Wrap login form so useSearchParams is allowed during prerender */}
+      <Suspense fallback={<div />}> 
+        <LoginForm />
+      </Suspense>
     </div>
-  )
+  );
 }
